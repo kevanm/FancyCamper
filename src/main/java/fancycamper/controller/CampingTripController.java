8a507f02435f36e95cc7d4e27943160ab7b379b5
Kevan <kevanmordan@gmail.com>
2018-04-24 19:44:07 -0700

    Updated the controller and created develop branch

--- src/main/java/fancycamper/controller/CampingTripController.java
+++ src/main/java/fancycamper/controller/CampingTripController.java
@@ -1,17 +1,30 @@
 package fancycamper.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import fancycamper.model.CampingTrip;
+import fancycamper.model.State;
+import fancycamper.service.CampingTripService;
+import java.util.List;
 
+@RestController
 public class CampingTripController {
+    private CampingTripService campingTripService;
 
-    @RestController
-    public class HelloController {
-
-        @RequestMapping("/")
-        public String index() {
-            return "Greetings from Spring Boot!";
-        }
-
+    public CampingTripController(CampingTripService campingTripService) {
+        this.campingTripService = campingTripService;
     }
+
+    @RequestMapping(value = "/api/trip/{id:.+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    private CampingTrip findOne(@PathVariable Long id) {
+        return campingTripService.findOne(id);
+    }
+
+    @RequestMapping("/api/trips/{state:.+}")
+    private List<CampingTrip> getAllSummariesForState(@PathVariable State state) {
+        return campingTripService.findByState(state);
+    }
+
 }
